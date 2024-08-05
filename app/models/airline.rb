134daff88637a853bee077f402d963854d50e645
Cory Bretsch <corybretsch@gmail.com>
2024-08-05 12:59:40 -0500

    last commit - just to make sure

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -2,15 +2,16 @@ class Airline < ApplicationRecord
   has_many :flights
   has_many :passengers, through: :flights
 
-  # def adult_passenger_list
+  def adult_passenger_list
+    passengers.where("passengers.age>=18").distinct
+  end
+end
+
+
+# def adult_passenger_list
   #   names = []
   #   passengers.where("passengers.age>=18").each do |passenger|
   #     names << passenger.name
   #   end
   #   names.uniq
   # end
-
-  def adult_passenger_list
-    passengers.where("passengers.age>=18").distinct
-  end
-end
