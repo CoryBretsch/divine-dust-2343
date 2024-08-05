40638172a30085ac6e89e9027abb4f752b212a67
Cory Bretsch <corybretsch@gmail.com>
2024-08-05 11:09:40 -0500

    user story 1 code and tests - will refactor with more robust testing if i have time at the end

---
+++ spec/features/flights/index_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.describe "flights index page" do 
+  it "can list all flight numbers, airline name, passengers names" do 
+    @airline = Airline.create!(name: "Spirit")
+    @flight1 = @airline.flights.create!(number: 735, 
+                                      date: "05/10/24", 
+                                      departure_city: "Nashville",
+                                      arrival_city: "New York")
+    @flight2 = @airline.flights.create!(number: 346, 
+                                      date: "08/10/24", 
+                                      departure_city: "San Diego",
+                                      arrival_city: "Milwaukee")
+    @passenger1 = Passenger.create!(name: "Diane", age: 87)
+    @passenger2 = Passenger.create!(name: "Laura", age: 63)
+    @passenger3 = Passenger.create!(name: "Dora", age: 12)
+    @passenger4 = Passenger.create!(name: "Millie", age: 38)
+    @passenger5 = Passenger.create!(name: "Leah", age: 10)
+    @passenger6 = Passenger.create!(name: "Humphry", age: 6)
+    FlightPassenger.create!(flight: @flight1, passenger: @passenger1)
+    FlightPassenger.create!(flight: @flight1, passenger: @passenger2)
+    FlightPassenger.create!(flight: @flight1, passenger: @passenger3)
+    FlightPassenger.create!(flight: @flight2, passenger: @passenger4)
+    FlightPassenger.create!(flight: @flight2, passenger: @passenger5)
+    FlightPassenger.create!(flight: @flight2, passenger: @passenger6)
+
+    visit flights_path
+  save_and_open_page
+    expect(page).to have_content("#{@flight1.number} - operated by: #{@flight1.airline_name}")
+    expect(page).to have_content("#{@flight2.number} - operated by: #{@flight2.airline_name}")
+
+    expect(page).to have_content(@passenger1.name)
+    expect(page).to have_content(@passenger2.name)
+    expect(page).to have_content(@passenger3.name)
+    expect(page).to have_content(@passenger4.name)
+    expect(page).to have_content(@passenger5.name)
+    expect(page).to have_content(@passenger6.name)
+    
+  end
+end
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe Flight, type: :model do
     it { should have_many :flight_passengers }
     it { should have_many(:passengers).through(:flight_passengers) }
   end
+
+  describe "instance methods" do 
+    it "can return flights #airline_name" do 
+    @airline = Airline.create!(name: "Spirit")
+    @flight1 = @airline.flights.create!(number: 735, 
+                                      date: "05/10/24", 
+                                      departure_city: "Nashville",
+                                      arrival_city: "New York")
+
+      expect(@flight1.airline_name).to eq("Spirit")
+    end
+  end
 end
